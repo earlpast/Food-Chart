2358977d4ce79b89df426b9887cd1a6403f98aaf
unknown <UPASTEA@EN-CNU3049WX5.PEROOT.COM>
2017-01-03 20:54:51 -0600

    Initial commit

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,28 @@
 $(document).foundation();
+$(function () {
+    $("#ckey .row").hide();
+    $("#ckey h3").click("live", showKey);
+    $(".callout").click("live", food);
+    $("#clearIt").click("live", clearAll);
+    console.log("Earl");
+});
+
+function showKey() {
+    console.log("Showkey");
+}
+
+function clearAll() {
+    $(".callout").removeClass("hitIt");
+}
+var portionsSelected = [];
+
+
+function food() {
+  var serving = getID($(this));
+$(this).addClass("hitIt");
+};
+
+function getID(x) {
+    var id = parseInt(x.attr('id').slice(-2).replace(/[a-zA-Z]+/g, ''));
+    return id
+}
